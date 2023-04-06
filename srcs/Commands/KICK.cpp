32e254e20ec0bd03f1dd3530fdcbb91badfef0fe
Killian MAMMERI <kmammeri@student.42lyon.fr>
2023-04-06 17:22:20 +0200

    fix kick op himsefl

--- srcs/Commands/KICK.cpp
+++ srcs/Commands/KICK.cpp
@@ -19,6 +19,16 @@ bool KICK::execute(Input const & cmd, Client * cli, IRCServer & serv) {
             }
             serv.getChannel(cmd.getTokens()[1])->sendToAll(msg + "\r\n");
             serv.getChannel(cmd.getTokens()[1])->removeUser(target);
+            if (serv.getChannel(cmd.getTokens()[1])->getUsers().size() != 0 && serv.getChannel(cmd.getTokens()[1])->getOperator() == cli->getNickname())
+            {
+                serv.getChannel(cmd.getTokens()[1])->setOperator(serv.getChannel(cmd.getTokens()[1])->getFirstUser());
+                serv.getChannel(cmd.getTokens()[1])->sendToAll(":" + cli->getNickname() + " MODE " + cmd.getTokens()[1] + " +o " + serv.getChannel(cmd.getTokens()[1])->getFirstUser() + "\r\n");
+                serv.getChannel(cmd.getTokens()[1])->sendToAll(":" + cli->getNickname() + " MODE " + cmd.getTokens()[1] + " -o " + cli->getNickname() + "\r\n");
+            }
+            if (serv.getChannel(cmd.getTokens()[1])->getUsers().size() == 0)
+            {
+                serv.deleteChannel(cmd.getTokens()[1]);
+            }
         }
         else
             cli->sendReply("401 "+ cli->getNickname() + " :No such nick/channel\r\n");
